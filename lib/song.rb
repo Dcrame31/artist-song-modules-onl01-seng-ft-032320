96fc350c62130034cc37c75688768816c2c3d760
Diane Crame <dcrame@icloud.com>
2020-04-07 23:16:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,17 @@
 require 'pry'
 
 class Song
-  extend Memorable
+  extend Memorable::ClassMethods
+include Memorable::InstanceMethods
   include Paramable
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  #def initialize
+   # @@songs << self
+  #end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
