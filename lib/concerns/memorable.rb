a6c65802a39be1986fbd5f9b8086ce310738a487
Diane Crame <dcrame@icloud.com>
2020-04-07 23:17:20 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,13 +1,13 @@
 module Memorable
   module ClassMethods
   def reset_all
-   all.clear
+      self.all.clear
+    end
+ 
+    def count
+      self.all.count
+    end
   end
-
-  def count
-   all.count
-  end
-end
 
   module InstanceMethods
     def initialize
